146a50e4ae7c4c10938eaff8864b42722ad95552
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2018-11-29 16:04:42 +0900

    Add eslint and some config

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,83 @@
 import * as React from 'react';
+import '../public/style.css';
 
 class App extends React.Component {
-    render(){
+    render() {
         return (
-           <div>Hello</div>
-        )
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="top">
+                        <div className="header">
+                            <h2>Wednesday, 22 Nov</h2>
+                            <span><i className="fas fa-plus"></i> </span>
+                        </div>
+                        <h3>3 tasks</h3>
+                    </div>
+                </div>
+                <div className="tasks-list">
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Molk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task" >
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Mng Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Walk</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                    <div className="task">
+                        <input type="checkbox" />
+                        <p className="task-name">Morning</p>
+                        <span className="time">7.00 am</span>
+                        <span className="line"></span>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,11 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
+            { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
         ]
     }
 }
